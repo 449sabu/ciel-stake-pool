1f7b3472b48fc2bd3ba683cde817eecd19fbf8aa
DESKTOP-JSJT2CJ\yogaw <449dev@gmail.com>
2022-10-13 22:46:18 +0900

    Remove Package PrelineUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,12 @@ module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
-    'node_modules/preline/dist/*.js',
   ],
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/typography'),
-    require('preline/plugin'),
     require("daisyui"),
   ],
   daisyui: {
