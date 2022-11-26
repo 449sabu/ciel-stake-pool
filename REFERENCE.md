d72dbf3383347998a6d5b67d52769c473812f2ea
DESKTOP-JSJT2CJ\yogaw <449dev@gmail.com>
2022-11-26 14:08:02 +0900

    Add GoogleAnalytics with Javascript

---
+++ REFERENCE.md
@@ -0,0 +1,2 @@
+## Add the GoogleAnalytics
+[Next.jsでGoogle Analyticsを使えるようにする](https://panda-program.com/posts/nextjs-google-analytics)  
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+import Head from 'next/head';
+import usePageView from '../../src/hooks/usePageView';
+import GoogleAnalytics from '../../src/libs/GoogleAnalytics';
 import Navbar from '../components/Navbar';
 import Footer from 'components/Footer';
+// Google Analytics
+// import { existsGaId, pageview } from '../../src/libs/gtag'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  usePageView();
+
   return (
     <>
+      <Head>
+        <GoogleAnalytics />
+      </Head>
       <header>
         <Navbar />
       </header>
