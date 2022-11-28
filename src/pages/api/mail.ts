0766e2c6ab9f5949659daa4874fc2036f4605910
DESKTOP-JSJT2CJ\yogaw <449dev@gmail.com>
2022-11-28 13:28:22 +0900

    Improve contact form

--- src/pages/api/mail.ts
+++ src/pages/api/mail.ts
@@ -1,18 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { createTransport } from 'nodemailer';
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+async function sendMail(req: NextApiRequest, res: NextApiResponse) {
   const transporter = createTransport({
     service: 'gmail',
     port: 465,
     secure: true,
     auth: {
-      user: process.env.MAIL_USER,
+      user: process.env.MAIL_TO,
       pass: process.env.MAIL_PASS,
     },
   });
   await transporter.sendMail({
-    from: process.env.MAIL_FROM,
+    from: process.env.MAIL_TO,
     to: process.env.MAIL_TO,
     subject: 'お問い合わせ',
     text: req.body,
@@ -21,4 +21,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({
     success: true,
   });
-};
+}
+
+export default sendMail;
